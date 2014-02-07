afe42d374bac73d8bdf141b562e2722a7b78ab3b
Team293 <293.first@gmail.com>
2014-02-07 15:26:51 -0500

    Quokka

--- AerialAssist/src/edu/wpi/first/wpilibj/templates/Ports.java
+++ AerialAssist/src/edu/wpi/first/wpilibj/templates/Ports.java
@@ -10,7 +10,7 @@ package edu.wpi.first.wpilibj.templates;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
- * @author NoamMiller
+ * @author Quokka
 =======
 >>>>>>> 90416743978d314ada11f7f8f98cfe77394a2c97
 =======
